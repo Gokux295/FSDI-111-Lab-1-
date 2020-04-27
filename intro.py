de764310cc1df5f73709b67d4415a45f210cdeed
tatur24 <54002798+tatur24@users.noreply.github.com>
2020-04-27 11:33:33 -0500

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,40 @@
+print ('hello world')
+
+
+name = "Chad"
+last = "Tatur"
+age = "34"
+found = False
+average = 2.34
+
+print (name)
+print(average)
+print(found)
+
+
+#Operations 
+
+print(21 + 21)
+print(100 + 100)
+print (12 * 321)
+print (100/10)
+print (10 % 3)
+
+print (name + name)
+print (age + age)
+print (name + str(age))
+
+def separator ():
+  print ('_____________________')
+  print('inside')
+
+print('outside')
+
+separator()
+
+if(age < 90)
+  print('you are still young')
+elif (age == 90)
+  print('you are borderline')
+else:
+  print('today is not your day')
